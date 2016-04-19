1a28b534ee99e98552dad7cd0da57f84475720f2
MrDarcys <MrDarcys@users.noreply.github.com>
2016-04-19 13:08:48 +0800

    Update Validator.js

--- Validator.js
+++ Validator.js
@@ -84,3 +84,11 @@ Validator.prototype.start = function(){
         }
     }
 };
+//实例化使用
+var validator = new Validator();
+validator.add(DOM,[{strategy: 'isNotEmpty',errorMsg:'errorMsg'});//可添加多个规则
+var errorMsg = validator.start(); // 获得效验结果
+if(errorMsg){
+    layer.msg(errorMsg,{icon: 7,time: 2000});
+    return false;
+}
